81979e92bc9a311a96f97643c700578a6944af3f
SOLVE-NITK <csd@nitk.edu.in>
2022-06-27 17:31:41 +0530

    updated experiments

---
+++ script.js
@@ -0,0 +1,18 @@
+const paths = [
+  {
+    id: 1,
+    path: "assets/exp-projection-of-planes/simulation/index.html",
+  },
+  {
+    id: 2,
+    path: "assets/exp-thermal-conductivity/simulation/index.html",
+  },
+  {
+    id: 3,
+    path: "assets/exp-coupled-pendulum/simulation/index.html",
+  },
+];
+
+const displaySimulator = (id) => {
+  document.getElementById("frame").src = `${paths[id - 1].path}`;
+};
